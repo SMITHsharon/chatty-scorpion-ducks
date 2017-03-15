08f35ab4a5ad78192232222403ce7e72fdf419e7
Latasha Simmons <lsimmons1832@gmail.com>
2017-03-15 16:45:05 -0500

    add framing for messageHandler iife

--- messageHandler.js
+++ messageHandler.js
@@ -1,2 +1,18 @@
-
 console.log("writing to console from messageHandler.js");
+// var userInput = document.getElementById('userInput');
+// var output = document.getElementById('output');
+// var clearBtn = document.getElementById('clearBtn');
+// var theme = document.getElementById('theme');
+// var size = document.getElementById('size');
+
+var Chatty = (function (oldChatty) {
+
+	return{
+		writeToDOM: function(write) {
+
+		},
+		editMgs: function (edit){
+
+		}
+	}
+});
