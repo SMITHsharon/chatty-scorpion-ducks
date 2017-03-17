8b579b50c4d1a05da37b317d0c283beb56e0a02c
sharon smith <sharon.e.smith2@gmail.com>
2017-03-16 19:47:28 -0500

    structure of delete.js

--- delete.js
+++ delete.js
@@ -1,2 +1,19 @@
 
-console.log("writing to console from delete.js");
+var Chatty = (function(){
+
+	function deleteMsg (xhrOneData){
+		// messageArray.push(msg1) // from getJSON.js
+		// waiting on <messageHandler> structure
+	}
+
+	function clearAllMsgs (xhrOneData){
+		// waiting on <messageHandler> structure
+	}
+
+	return {
+		// getMessageArray: function(){
+			return messageArray;
+		}
+	}
+
+})(Chatty);
