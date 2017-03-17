a2413ea54efe4eb93dde14ae1965987f45fd48ba
Latasha Simmons <lsimmons1832@gmail.com>
2017-03-16 19:35:12 -0500

    save work

--- messageHandler.js
+++ messageHandler.js
@@ -1,20 +1,15 @@
-console.log("writing to console from messageHandler.js");
-// var userInput = document.getElementById('userInput');
-// var clearBtn = document.getElementById('clearBtn');
-// var theme = document.getElementById('theme');
-// var size = document.getElementById('size');
-// var output = document.getElementById('output');
-
 var Chatty = (function (oldChatty) {
 
 	
 		oldChatty.writeToDOM =  function(message) {
-				return messageArray[message]
-
-		},
+			console.log("I'm inside write");
+			var message += messageArray;
+				return message;
+		};
 		oldChatty.editMgs = function (){
 
 		}
 	};
-	return oldChatty
+	return oldChatty;
+
 })(Chatty || {});
