baa8e60228b822915d0afb30756920247246ca38
jmarcusgill <jmarcusgill@gmail.com>
2017-03-18 11:54:07 -0500

    merge conflict

--- messageHandler.js
+++ messageHandler.js
@@ -1,25 +1,26 @@
 var Chatty = (function (oldChatty) {
 		oldChatty.addUserInput = function(input){
 			var message = oldChatty.getMessageArray();
-
-			console.log(message);
-			
 			message.push(input);
 			oldChatty.writeToDOM(message);
-			
+			return message;
 		}
 
 		oldChatty.writeToDOM =  function (messageArray) {
 			var messageElement = "";
 				for (var i = 0; i < messageArray.length; i++) {
 					var captureMessage = messageArray[i];
-					messageElement += `<div class="mssgText">${captureMessage}<button class="delMsg">Delete</button><button class="edMsg">Edit</button></div>`;
+					messageElement += `<div class="mssgText">${captureMessage}<button class="delMsg" id = "delMsg${i}">Delete</button><button class="readMsg"><img src="http://franriavilla.in/images/unmute.png"></button></div>`;
 				}
 				output.innerHTML = messageElement;
 		};
 		
-		oldChatty.editMgs = function (){
-
+		oldChatty.editMsg = function (editEvent){
+			for (var j=0; j<message.lenght; j++){
+				if (messageArray[j]===editEvent ){
+					console.log(editEvent, "replace text")
+				};
+			}
 		}
 	return oldChatty;
 
