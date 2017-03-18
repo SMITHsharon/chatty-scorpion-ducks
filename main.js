29c591428b949cafcb65c540887749873de53ce3
Sanyyouisf <sanymamdoh@yahoo.com>
2017-03-17 19:48:49 -0500

    some details

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 
-console.log("writing to console from main.js");
+// console.log("writing to console from main.js");
+// var Chatty = (function(){
+
+// 	return 
+// });
+
+
+// Function to detect enter key
+var detectEnter = document.getElementById("userInput");
+
+//variable to hold user input
+var captureInput = detectEnter.value;
+
+detectEnter.addEventListener("keypress", function(event){
+    if(event.keyCode === 13){
+    	
+    	console.log(event);
+    	console.log("I detected the enter key");
+		console.log(detectEnter.innetHTML);
+
+       // Chatty.messageHandler(writeToDOM); //writeToDOM is just a placeholder
+    }
+});
+
+
+
+
+// Function to detect click of the Clear All Messages button
+// var clearAllMsg = document.getElementById("clearBtn");
+
+// clearAllMsg.addEventListener("click", function(){
+// 		Chatty.delete(clearAllMsgs);
+// });
